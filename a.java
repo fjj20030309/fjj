7d2900bcce8fffc96ad7d9c02b2eefa4e93f2ca2
junjie <1574447288@qq.com>
2022-09-25 19:10:44 +0800

    a.java

---
+++ a.java
@@ -0,0 +1,13 @@
+package ex;
+
+public class a {
+	public static void main(String []args) {
+		int i = 5;
+		int j = 3;
+		int a=6;
+		int max = i > j ? a+=i : j;
+		System.out.println(max);
+		
+	}
+
+}
